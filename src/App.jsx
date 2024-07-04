bbab988659b929e04227eb6645d3b6403633463f
yusufalp <alpyusuf007@gmail.com>
2024-07-04 14:12:59 -0700

    add options to connect an account

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,16 @@ import { Switch, Route } from "react-router-dom/cjs/react-router-dom";
 import Home from "./components/Home/Home";
 import Login from "./components/Login/Login";
 import Signup from "./components/Signup/Signup";
+import Connect from "./components/Connect/Connect";
 
 function App() {
   return (
     <Switch>
       <Route exact path="/">
         <Home />
+      </Route>
+      <Route path="/connect">
+        <Connect />
       </Route>
       <Route path="/login">
         <Login />
