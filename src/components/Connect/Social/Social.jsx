365a4775c410d9c7aaab462e1afe30badee061e9
yusufalp <alpyusuf007@gmail.com>
2024-07-06 21:58:32 -0700

    handle google authentication

--- src/components/Connect/Social/Social.jsx
+++ src/components/Connect/Social/Social.jsx
@@ -3,8 +3,25 @@ import PropTypes from "prop-types";
 import { capitalize } from "../../../utils";
 
 function Social({ media }) {
+  const handleSocialSignup = async () => {
+    try {
+      const response = await fetch(`http://localhost:8080/api/auth/${media}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
-    <button className="w-96 bg-red-500 hover:bg-red-600 border-2 text-white text-lg py-2 px-4 rounded-md shadow-sm">
+    <button
+      onClick={handleSocialSignup}
+      className="w-96 bg-red-500 hover:bg-red-600 border-2 text-white text-lg py-2 px-4 rounded-md shadow-sm"
+    >
       Continue with {capitalize(media)}
     </button>
   );
