b6e36fcd642513268c08522a508bc1a1fa6be5ed
yusufalp <alpyusuf007@gmail.com>
2024-07-09 22:41:00 -0700

    update folder structure

--- src/components/Connect/Signup/Signup.jsx
+++ src/components/Connect/Signup/Signup.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useHistory } from "react-router-dom";
 
-import { ROLES } from "../../../utils/constants";
+import { ROLES } from "../../../constants";
 import { capitalize } from "../../../utils";
 
 function Signup() {
