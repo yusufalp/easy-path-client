d6cb6335d529b80f7a730a3be0e4719d1cf6d6b5
yusufalp <alpyusuf007@gmail.com>
2024-07-09 21:32:01 -0700

    setup redux and implement auth reducers

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom/cjs/react-router-dom.min.js";
+import { Provider } from "react-redux";
+
+import store from "./store/store.js";
 
 import App from "./App.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
