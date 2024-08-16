2321b0948d3353c7a57070329029e0c15c591c21
yusufalp <alpyusuf007@gmail.com>
2024-08-16 11:31:49 -0700

    separate imports

--- src/shared/Header/Header.jsx
+++ src/shared/Header/Header.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
+
 import { logout } from "../../store/features/authSlice";
 
 function Header() {
